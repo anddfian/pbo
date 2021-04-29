7be06fade33b08d113f0a38dbe56cb47c6053a99
Andi Alfian Bahtiar <55624363+anddfian@users.noreply.github.com>
2021-04-29 16:58:41 +0800

    Delete Posttest 5 directory

--- Posttest 5/src/dicoding/Education.java
+++
@@ -1,6 +0,0 @@
-package dicoding;
-
-public abstract class Education {
-    protected abstract void show(String name, String description, int classAcademy);
-    protected abstract void show(int step, String name, String description, String level, int time, String technology);
-}
